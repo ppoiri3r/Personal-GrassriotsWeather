abbe72d5022b22b3d8d5e35e180cf0e027fb84af
ppoiri3r <paula-poirier@hotmail.com>
2024-03-24 17:50:06 -0400

    restructured code for better organization. moved newURLSearchParams into the getTheWeather function. getTheWeather function is now called in the init function of the app, because by the time it runs we've already fetched coordinates and dates

--- scripts.js
+++ scripts.js
@@ -1,65 +1,88 @@
 // define weatherApp object
 const weatherApp = {};
+// define utilities object.
+// this object will house utility functions/helper functions. 
+const utilities = {};
 
-// initialize URLs for weather and geolocation APIs
 weatherApp.weatherURL = 'https://api.open-meteo.com/v1/forecast';
 weatherApp.geolocationURL = 'https://services.grassriots.io/';
-
-// initialize variables
-weatherApp.todaysDate;
-weatherApp.endDate;
-weatherApp.lat;
-weatherApp.long;
 weatherApp.weatherContainer = document.querySelector('#weatherContainer');
 
-// define search parameters for the weather API
-weatherApp.searchParams = new URLSearchParams({
-  "latitude": weatherApp.lat,
-  "longitude": weatherApp.long,
-  "temperature_unit": "celsius",
-  "wind_speed_unit": "mph",
-  "precipitation_unit": "mm",
-  "start_date": weatherApp.todaysDate,
-  "end_date": weatherApp.endDate,
-  "timezone": "auto",
-  "current": ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "is_day", "precipitation", "rain", "showers", "snowfall", "weather_code", "cloud_cover", "pressure_msl", "surface_pressure", "wind_speed_10m", "wind_direction_10m", "wind_gusts_10m"],
-  "hourly": "temperature_2m",
-  "daily": ["weather_code", "apparent_temperature_max", "apparent_temperature_min", "precipitation_probability_mean", "sunrise", "sunset", "wind_speed_10m_max", "wind_speed_10m_min"]
-})
-
-// function to hide javascript disabled message and call on the geolocateUser function
-weatherApp.hideJsDisabledMessage = function() {
+// function to hide javascript disabled message
+utilities.hideJsDisabledMessage = function() {
   const jsDisabledMessage = document.querySelector('#js-disabled-message');
-  const loadingSpinner = document.querySelector('.lds-ring');
   jsDisabledMessage.classList.add('hidden');
-  loadingSpinner.classList.remove('hidden');
+}
 
-  weatherApp.geolocateUser();
+// this is called in the getTheWeather function to show a loading animation while we fetch the data.
+utilities.showLoadingAnimation = function() {
+  const loadingSpinner = document.querySelector('.lds-ring');
+  if (loadingSpinner) {
+    loadingSpinner.classList.remove('hidden');
+  }
+}
+
+// whether data retrieval is successful or not, we need to hide the animation and either display weather info or that an error has occurred. 
+utilities.hideLoadingAnimation = function() {
+  const loadingSpinner = document.querySelector('.lds-ring');
+
+  if (loadingSpinner) {
+    loadingSpinner.style.transition = 'opacity 0.5s ease';
+    loadingSpinner.style.opacity = '0';
+
+    // hide the spinner after the animation ends
+    setTimeout(() => {
+      loadingSpinner.style.display = 'none';
+    }, 500);
+  }
+}
+
+// this function is called in the displayWeather function to display the date in long date format.
+// ie. Friday, March 22, 2024 (except we're not displaying the year here)
+utilities.formatDate = function(dateString) {
+    const date = new Date(dateString);
+    const options = { weekday: 'long', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+}
+
+// this function is called in the displayWeather function to display the time in the 12-hour clock format.
+// ie. 3:04PM.
+utilities.formatTime = function(timeString) {
+    const date = new Date(timeString);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let meridiem = hours >= 12 ? 'PM' : 'AM';
+
+    // convert hours to 12-hour format
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
+
+    return hours + ':' + formattedMinutes + meridiem;
 }
 
 // function to fetch the users coordinates, city and region 
 weatherApp.geolocateUser = async function() {
+  utilities.showLoadingAnimation();
   try {
     const response = await fetch(weatherApp.geolocationURL);
     if (!response.ok) {
       throw new Error('Network response from fetching geolocationURL was not ok:' + response.statusText);
     }
-    const data = await response.json();
-    if (!data.data || !data.data.lat || !data.data.lng) {
+    const jsonResponse = await response.json();
+    if (!jsonResponse.data || !jsonResponse.data.lat || !jsonResponse.data.lng) {
       throw new Error('Invalid data received from geolocationURL');
     } 
     // on success, save geolocation data into variables that we'll pass to the getTheWeather function.
-    const isocode = data.data.isocode;
-    const region = data.data.englishRegionName;
-    const city = data.data.city;
-    weatherApp.lat = data.data.lat;
-    weatherApp.long = data.data.lng;
-    const { todaysDate, endDate } = weatherApp.getDates();
-
-    weatherApp.getTheWeather(region, city);
+    weatherApp.lat = jsonResponse.data.lat;
+    weatherApp.long = jsonResponse.data.lng;
+    weatherApp.city = jsonResponse.data.city;
+    weatherApp.region = jsonResponse.data.englishRegionName;
+    weatherApp.isocode = jsonResponse.data.isocode;
   } catch (error) {
     // this code will execute if there's a network response error or the json data returned is invalid
-    console.error('Error fetching users coordinates:', error);
+    console.error('Error fetching users coordinates in geolocateUser function:', error);
     weatherApp.hideWeatherContainer();
     weatherApp.displayErrorMessage();
   }
@@ -75,48 +98,24 @@ weatherApp.getDates = function() {
   weatherApp.todaysDate = y + '-' + m + '-' + d;
 
   let endDate = new Date(today);
+  // should we want to getch more than the 7 days forecast, we only need to change the "+ 7" in this line of code to + [desired number]. 
+  // we can fetch up to a 16 day forecast with this particular weather api. 
   endDate.setDate(endDate.getDate() + 7);
   d = String(endDate.getDate()).padStart(2, '0');
   m = String(endDate.getMonth() + 1).padStart(2, '0');
   y = endDate.getFullYear();
   weatherApp.endDate = y + '-' + m + '-' + d;
-
-  return { todaysDate: weatherApp.todaysDate, endDate: weatherApp.endDate };
-}
-
-// this function is called in the displayWeather function to display the date in long date format.
-// ie. Friday, March 22, 2024 (except we're not displaying the year here)
-function formatDate(dateString) {
-    const date = new Date(dateString);
-    const options = { weekday: 'long', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
-}
-
-// this function is called in the displayWeather function to display the time in the 12-hour clock format.
-// ie. 3:04PM.
-function formatTime(timeString) {
-    const date = new Date(timeString);
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let meridiem = hours >= 12 ? 'PM' : 'AM';
-
-    // convert hours to 12-hour format
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-
-    const formattedMinutes = minutes < 10 ? '0' + minutes : minutes;
-
-    return hours + ':' + formattedMinutes + meridiem;
 }
 
 // this function is called on success of geolocating data retrieval in the geolocateUser function. 
 // the geolocateUser passes information about the users region and city, which we'll subsequently pass to the displayTheWeather function that is called on success of weather data retrieval here.
-weatherApp.getTheWeather = async function(region, city) {
+weatherApp.getTheWeather = async function() {
   try {
-    weatherApp.showLoadingAnimation();
-    const urlObj = new URL(weatherApp.weatherURL);
-    // defining our new search parameters that we'll add onto our base URL
-    const searchParams = new URLSearchParams({
+    await weatherApp.geolocateUser(); 
+    await weatherApp.getDates();
+    
+    const weatherURLObj = new URL(weatherApp.weatherURL);
+    weatherApp.searchParams = new URLSearchParams({
       "latitude": weatherApp.lat,
       "longitude": weatherApp.long,
       "temperature_unit": "celsius",
@@ -130,64 +129,39 @@ weatherApp.getTheWeather = async function(region, city) {
       "daily": ["weather_code", "apparent_temperature_max", "apparent_temperature_min", "precipitation_probability_mean", "sunrise", "sunset", "wind_speed_10m_max", "wind_speed_10m_min"]
     })
 
-    urlObj.search = searchParams.toString();
+    console.log('weatherURLObj:', weatherURLObj);
+    weatherURLObj.search = weatherApp.searchParams.toString();
 
-    const response = await fetch(urlObj);
+    const response = await fetch(weatherURLObj);
     // if the network response is not ok, throw an error
     if (!response.ok) {
-      throw new Error('Network response from fetching urlObj was not ok:' + response.statusText);
+      throw new Error('Network response from fetching weatherURLObj was not ok:' + response.statusText);
     }
     // if the network response is ok, parse the response as a json object
-    const data = await response.json();
+    const jsonResponse = await response.json();
     // if the data is invalid, throw an error. 
-    if (!data) {
+    if (!jsonResponse) {
       throw new Error('Invalid data received from urlObj');
     } 
     // if everything is successful, hide the loading animation 
     // console log our response. 
-    weatherApp.hideLoadingAnimation(); 
-    console.log('Weather data:', data);
+    console.log('Weather data:', jsonResponse);
+    utilities.hideLoadingAnimation(); 
 
     // this set timeout will fade the weather smoothly once the loading spinner is done fading out.
     setTimeout(() => {
-      weatherApp.displayWeather(data, city, region);
+      weatherApp.displayWeather(jsonResponse);
     }, 500);
 
   } catch (error) {
     // this code will execute should the network response fail or data be invalid. 
     console.error('Error fetching weather data:', error);
-    weatherApp.hideLoadingAnimation(); 
+    utilities.hideLoadingAnimation(); 
     weatherApp.hideWeatherContainer();
     weatherApp.displayErrorMessage();
   }
 }
 
-// this is called in the getTheWeather function to show a loading animation while we fetch the data.
-weatherApp.showLoadingAnimation = function() {
-  const loadingSpinner = document.querySelector('.lds-ring');
-  if (loadingSpinner) {
-    loadingSpinner.style.display = 'block';
-  }
-}
-
-// whether data retrieval is successful or not, we need to hide the animation and either display weather info or that an error has occurred. 
-weatherApp.hideLoadingAnimation = function() {
-  const loadingSpinner = document.querySelector('.lds-ring');
-
-  if (loadingSpinner) {
-    loadingSpinner.style.transition = 'opacity 0.5s ease';
-    loadingSpinner.style.opacity = '0';
-
-    // hide the spinner after the animation ends
-    setTimeout(() => {
-      loadingSpinner.style.display = 'none';
-    }, 500);
-  }
-}
-
-// dynamically populate the error message fields. 
-// as the code is right now, this doesn't have to be dynamically inserted. 
-// i've dynamically inserted it here as i'd like to add some condition that presents a different error depending on whether this was an error while fetching their coordinates or while fetching the weather. the geolocation error would come first as the function is called first.
 weatherApp.displayErrorMessage = function () {
   const h1El = document.querySelector('#errorContainer h1');
   const errorPEl = document.querySelector('#errorContainer p');
@@ -220,9 +194,9 @@ weatherApp.displayWeather = function(data, city, region) {
   const rainEl = document.querySelector('#rain');
   const sunsetEl = document.querySelector('#sunset');
 
-  let formattedDateData = formatDate(weatherApp.todaysDate);
-  let formattedSunriseTime = formatTime(data.daily.sunrise[0]);
-  let formattedSunsetTime = formatTime(data.daily.sunset[0]);
+  let formattedDateData = utilities.formatDate(weatherApp.todaysDate);
+  let formattedSunriseTime = utilities.formatTime(data.daily.sunrise[0]);
+  let formattedSunsetTime = utilities.formatTime(data.daily.sunset[0]);
   let tempData = data.current.temperature_2m;
   let appTempData = data.current.apparent_temperature;
   let tempDataUnit = data.current_units.apparent_temperature;
@@ -254,7 +228,8 @@ weatherApp.displayWeather = function(data, city, region) {
 // this kicks off everything by checking whether the user has their javascript disabled, 
 // if they do, we display a message to let them know this app won't run unless they enable it. 
 weatherApp.init = function() {
-  weatherApp.hideJsDisabledMessage();
+  utilities.hideJsDisabledMessage();
+  weatherApp.getTheWeather();
 }
 
 weatherApp.init();
